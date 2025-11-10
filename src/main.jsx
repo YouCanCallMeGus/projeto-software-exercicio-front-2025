323d97ab2d6c0816540a9954fa34d2b561f9fa49
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2025-11-09 23:35:30 -0300

    auth

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="4rwe12yYJbxAD122EDkjl61unHBaHFL0"
+      domain="dev-2tmvsgf0y3tgkhni.us.auth0.com"
+      clientId="U47nLS4wytcchK3kHEa4xpYG147SYbjW"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-2tmvsgf0y3tgkhni.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
